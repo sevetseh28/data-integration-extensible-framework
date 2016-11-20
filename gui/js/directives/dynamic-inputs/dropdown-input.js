cca096388e5b6401df91b74808ceaac3aec15908
sevetseh28 <hernaner28@gmail.com>
2016-11-20 15:24:20 -0300

    Added example for dropdown input.

--- gui/js/directives/dynamic-inputs/dropdown-input.js
+++ gui/js/directives/dynamic-inputs/dropdown-input.js
@@ -10,3 +10,43 @@ angular.module("materialAdmin")
 
         }
     });
+
+// EXAMPLE OF A DROPDOWN
+// 'inputName': {
+//     'type': 'dropdown',
+//         'label': 'Select one option',
+//         'selectedoption': {  },
+//     'options': [
+//         {
+//             'label': 'esto es un slider y un text input',
+//             'config': {
+//                 'input1': {
+//                     "type": "slider",
+//                     "label": "Chus a namber",
+//                     "value": 0.5,
+//                     "start": 0,
+//                     "end": 1,
+//                     "step": 0.1,
+//                     "color": "amber"
+//                 },
+//                 'path': {
+//                     'type': 'text',
+//                     'name': 'Ponga el path'
+//                 }
+//             }
+//         },
+//         {
+//             'label': 'esto es un checkbox',
+//             'config': {
+//                 'input2': {
+//                     "type": "checkbox",
+//                     "label": "Check some boxes",
+//                     "options": [
+//                         {"label": "Opcion 1", "value": false},
+//                         {"label": "Opcion 2", "value": false}
+//                     ]
+//                 }
+//             }
+//         }
+//     ]
+// }
