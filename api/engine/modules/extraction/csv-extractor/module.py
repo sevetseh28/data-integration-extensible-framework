4e972b9023273d8ce10a43f9ed2da04127903807
Nico <nicotomeo5@gmail.com>
2016-11-28 02:06:36 -0300

    Csv extractor, test y csv de prueba

---
+++ api/engine/modules/extraction/csv-extractor/module.py
@@ -0,0 +1,58 @@
+
+from engine.models.record import *
+from engine.modules.extraction.extraction_module import ExtractionModule
+from engine.modules.module import Module
+import csv
+
+class CsvExtractor(ExtractionModule):
+
+    """
+        Formato config:
+        {
+            'pathcsv': "[host]"
+
+        }
+    """
+
+    def __init__(self, **kwargs):
+        super(CsvExtractor, self).__init__(**kwargs)
+        self.pretty_name = 'CsvExtractor'
+        self.pathcsv = self.config['pathcsv']
+
+    def run(self):
+
+        with open(self.pathcsv) as csvfile:
+            reader = csv.DictReader(csvfile)
+            for fieldname in reader.fieldnames:
+                self.add_to_schema(Column(fieldname))
+            for i,row in enumerate(reader):
+                self.records.append(Record())
+                for key,value in row.items():
+                    column = Column(key)
+                    column.fields.append(get_field_from_csv(value))
+                    self.records[i].columns[column.name] = column
+        return self.schema, self.records
+
+
+
+
+
+def ignore_exception(IgnoreException=Exception,DefaultVal=None):
+    def dec(function):
+        def _dec(*args, **kwargs):
+            try:
+                return function(*args, **kwargs)
+            except IgnoreException:
+                return DefaultVal
+        return _dec
+    return dec
+
+
+def get_field_from_csv(value):
+    checkfloat = ignore_exception()(float)
+    floatvalue = checkfloat(value)
+    if floatvalue is not None:
+        return Field(floatvalue,FieldType.number)
+    else:
+        return Field(value,FieldType.string)
+
